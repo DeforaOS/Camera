4d9f261e62f6c7d43f85e347ed4551f0658535d8
Pierre Pronchery <khorben@defora.org>
2013-06-29 18:01:49 -0400

    More consistent file headers

--- src/camera.h
+++ src/camera.h
@@ -1,6 +1,6 @@
 /* $Id$ */
 /* Copyright (c) 2012-2013 Pierre Pronchery <khorben@defora.org> */
-/* This file is part of DeforaOS desktop camera */
+/* This file is part of DeforaOS Desktop Camera */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
