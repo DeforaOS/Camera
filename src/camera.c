ff75e814410f787997b7b77a9b508e54b1810a7b
Pierre Pronchery <khorben@defora.org>
2015-05-23 19:38:24 +0200

    Avoid a couple warnings

--- src/camera.c
+++ src/camera.c
@@ -1,19 +1,17 @@
 /* $Id$ */
-static char const _copyright[] =
-"Copyright © 2012-2015 Pierre Pronchery <khorben@defora.org>";
+/* Copyright (c) 2012-2015 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Desktop Camera */
-static char const _license[] =
-"This program is free software: you can redistribute it and/or modify\n"
-"it under the terms of the GNU General Public License as published by\n"
-"the Free Software Foundation, version 3 of the License.\n"
-"\n"
-"This program is distributed in the hope that it will be useful,\n"
-"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-"GNU General Public License for more details.\n"
-"\n"
-"You should have received a copy of the GNU General Public License\n"
-"along with this program.  If not, see <http://www.gnu.org/licenses/>.";
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 
 
